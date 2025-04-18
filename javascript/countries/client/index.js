6289b9b3ada6249ac0b2509880745ae027f92071
h4wks123 <ivanne_bayer@LAPTOP-31P56QLH.>
2025-04-18 23:43:31 +0800

    feat: added display for countries with filtering

--- javascript/countries/client/index.js
+++ javascript/countries/client/index.js
@@ -1,68 +1,87 @@
-let getCountriesData = new Promise((resolve, reject) => {
-  const url = "https://restcountries.com/v3.1/all";
-  fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          "Network error, could not connect to countries endpoint"
-        );
-      }
-      resolve(response.json());
-    })
-    .catch((error) => {
-      reject(error.message);
-    });
+let filter = "all";
+let searchCountry = document.getElementById("filter-options-search__text");
+let regionCountry = document.getElementById("filter-options-region");
+searchCountry.addEventListener("change", (e) => {
+  filter = e.target.value === "" ? "all" : `name/${e.target.value}`;
+  handleCountryFetch(filter);
+});
+regionCountry.addEventListener("change", (e) => {
+  filter = e.target.value === "" ? "all" : `region/${e.target.value}`;
+  handleCountryFetch(filter);
 });
 
-function parseCountriesData(data) {
-  let countriesData = [];
-  for (let index = 0; index < data.length; index++) {
-    countriesData.push([
-      data[index].flags.png,
-      data[index].name.common,
-      data[index].population,
-      data[index].region,
-      data[index].capital,
-    ]);
-  }
-  countriesData = countriesData.sort((a, b) => a[1] - b[1]);
-  displayCountriesCard(countriesData);
-}
-
-function displayCountriesCard(countriesData) {
-  let printedCountriesData = [];
-  for (let index = 0; index < countriesData.length; index++) {
-    printedCountriesData.push(`
-        <article>
-            <img src="${countriesData[index][0]}" />
-            <h4>${countriesData[index][1]}</h4>
-            <h6>${countriesData[index][2]}</h6>
-            <h6>${countriesData[index][3]}</h6>
-            <h6>${countriesData[index][4]}</h6>
-        </article>
-        `);
-  }
-  console.log(printedCountriesData);
-}
-
-getCountriesData
-  .then((data) => {
-    parseCountriesData(data);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log("operations are now completed!");
+function fetchCountriesData(filter) {
+  return new Promise((resolve, reject) => {
+    const url = `https://restcountries.com/v3.1/${filter}`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(
+            "Network error, could not connect to countries endpoint"
+          );
+        }
+        resolve(response.json());
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
   });
+}
+
+function displayCountriesData(data) {
+  let countriesContainer = document.getElementById("country-cards");
+  let fragment = document.createDocumentFragment();
+
+  countriesContainer.innerHTML = "";
+
+  for (let index = 0; index < data.length; index++) {
+    let card = document.createElement("div");
+    card.className = "country-cards__paper";
+    card.innerHTML += `
+        <img class="country-cards__paper-flag" loading="lazy" src="${data[index].flags.png}" />
+        <article class="country-cards__paper-details">
+          <h4 class="country-cards__paper-details-name">${data[index].name.common}</h4>
+          <h6 class="country-cards__paper-details-population"><p style="font-weight: bold;">Population: </p> ${data[index].population}</h6>
+          <h6 class="country-cards__paper-details-region"><p style="font-weight: bold;">Region: </p> ${data[index].region}</h6>
+          <h6 class="country-cards__paper-details-capital"><p style="font-weight: bold;">Capital: </p> ${data[index].capital}</h6>
+        </article>
+        `;
+    fragment.appendChild(card);
+  }
+
+  countriesContainer.appendChild(fragment);
+}
+
+function handleCountryFetch(filter) {
+  fetchCountriesData(filter)
+    .then((data) => {
+      displayCountriesData(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      console.log("operations are now completed!");
+    });
+}
 
 function changeTheme() {
-  document.body.classList.toggle("dark");
-  if (document.body.classList.contains("dark")) {
-    document.getElementById("navigation-bar-right__button-mode").src =
-      "./assets/light-mode.svg";
-  } else {
+  document.body.classList.toggle("light");
+  if (document.body.classList.contains("light")) {
     document.getElementById("navigation-bar-right__button-mode").src =
       "./assets/dark-mode.svg";
+    document.getElementById("navigation-bar-right__text").innerHTML =
+      "Dark Mode";
+    document.getElementById("filter-options-search__icon").src =
+      "./assets/search-dark-mode.svg";
+  } else {
+    document.getElementById("navigation-bar-right__button-mode").src =
+      "./assets/light-mode.svg";
+    document.getElementById("navigation-bar-right__text").innerHTML =
+      "Light Mode";
+    document.getElementById("filter-options-search__icon").src =
+      "./assets/search-light-mode.svg";
   }
 }
+
+handleCountryFetch(filter);
